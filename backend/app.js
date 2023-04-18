abfa5ed9c653c0ca52d5ad38d7bfe84074d8dd0a
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-18 20:57:51 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send = "API is working";
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
